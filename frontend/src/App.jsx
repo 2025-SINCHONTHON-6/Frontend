cb3feb1f1cd2a7d20bfda1c6b5009203311fd714
geummi0520 <rmaal520@gmail.com>
2025-09-06 02:11:45 +0900

    기록부분 수정#1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import "./index.css";
 import { Routes, Route } from "react-router-dom";
-import MainLayout from "@/layouts/mainLayout";
-import Test from "@/pages/test";
+import MainLayout from "./layouts/mainLayout";
+import Test from "./pages/test";
+import Record from "./pages/record/record";
+import RecordDetail from "./pages/record/recordDetail";
+import RecordWrite from "./pages/record/recordWrite";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Test />} />
+        <Route path="/record" element={<Record />} />
+        <Route path="/record/write" element={<RecordWrite/>} />
+        <Route path="/record/detail" element={<RecordDetail/>} />
         {/* 페이지들 */}
       </Route>
     </Routes>
