12e140206074ca9e947ca9e7b41dc0e9ef0efd07
LeeYonDu <gj5051129@gmail.com>
2025-09-06 07:29:14 +0900

    record 페이지 챌린지판 추가

--- frontend/src/components/record/challenge.jsx
+++ frontend/src/components/record/challenge.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 
 export default function Challenge() {
   return (
     <div className="min-h-[100dvh] w-auto bg-black flex justify-center">
       <main className="relative w-full max-w-[390px] bg-white">
-        <Outlet />
+        <div className='flex justify-start items-center flex-col gap-3'>
+            <img src="/svg/challenge1.svg" alt="chellengeTab" />
+            <img src="/svg/challenge2.svg" alt="chellengeTab" />
+            </div>
       </main>
     </div>
   );
